3b02fc4b755ccccb17add1955ae4f20a94cd62ef
Puneeth <koduripuneethkumar@gmail.com>
2022-01-21 10:54:05 +0530

    adding intial commit

---
+++ src/main/java/com/democompany2/new_demo2_project/App.java
@@ -0,0 +1,13 @@
+package com.democompany2.new_demo2_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
